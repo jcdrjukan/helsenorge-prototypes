15c5c43e868dc119d3f0ff0d9ecf7ca88ac7dda1
jcdrjukan <jcdana@gmail.com>
2026-08-28 12:09:31 +0200

    Behandlingshjelpemidler: match Oppsummering item format to historical-orders convention (name x qty stk.)

--- frankenstein/src/behandlingshjelpemidler/Step4.tsx
+++ frankenstein/src/behandlingshjelpemidler/Step4.tsx
@@ -67,7 +67,7 @@ export default function Step4({ equipment, quantities, delivery, comment, onSubm
             <ul className="order-summary-list">
               {selectedItems.map((item, i) => (
                 <li key={i} style={{ font: 'var(--mobile-body)' }}>
-                  {item.qty}x {item.consumableName} til {item.eqName}
+                  {item.consumableName} x {item.qty} stk. til {item.eqName}
                 </li>
               ))}
             </ul>
